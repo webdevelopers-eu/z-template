3f06f065bdc34bfbbfb6c7143ff5db7c09616824
MAD <mad@webdevelopers.eu>
2019-12-31 16:26:19 +0100

    Hidding/showing is made using adding/removing of 'dna-template-hidden' class to avoid problems with setting 'display' CSS property.

--- jquery.template.js
+++ jquery.template.js
@@ -246,7 +246,16 @@ $.fn.template = function(vars, inPlace) {
 		    }
 		});
 
-		if (show !== null) $(el)[show ? 'show' : 'hide']();
+		if (show !== null) {
+		    if (show) { // we go with css class, because determining the needed "display" CSS property is problematic
+			$(el).removeClass('dna-template-hidden');
+			if (el.style.display == 'none') {
+			    $(el).css('display', '');
+			}
+		    } else {
+			$(el).addClass('dna-template-hidden');
+		    }
+		}
 	    });
 
 	// Subtemplates
